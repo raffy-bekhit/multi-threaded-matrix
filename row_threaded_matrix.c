46b75e30af1ef99d01d3a79b644d207c2bd27626
raffy-bekhit <raffybekhit@gmail.com>
2017-11-21 16:30:05 +0200

    implement functioning row_threaded_matrix_multiplication

--- row_threaded_matrix.c
+++ row_threaded_matrix.c
@@ -20,15 +20,20 @@ void* calculate_row(int* );                        //prototypes for the used fun
 
 int main()
 {
+
     int i,j;                                               //loop for assigning a thread to each and every element that will be calculated in the new matrix
     int error_flag;
     pthread_t threads_array[M];
     pthread_attr_t attr_array[M];
+    int rows[M];
+
+
     for(i = 0; i < M; i++)
     {
+        rows[i]=i;
 
         pthread_attr_init(&attr_array[i]);    //get default attributes
-        error_flag = pthread_create(&threads_array[i],&attr_array[i],calculate_row,i); //create the
+        error_flag = pthread_create(&threads_array[i],&attr_array[i],calculate_row,&rows[i]); //create the
 
         if(error_flag!=0)
         {
@@ -72,10 +77,10 @@ void* calculate_row(int* row)
 
     for(i=0; i< matrixB_columns; i++) //loops the columns of matrixB
     {
+
         for(j=0; j< matrixB_rows; j++) //loops rows of matrixB //loops columns of matrixA for a specific row
         {
-            sum=sum + ((matrixA[*row][j]) * matrixB[j][i]);
-
+            sum = sum + (matrixA[*row][j] * matrixB[j][i]);
         }
         matrixC[*row][i]=sum;
         sum=0;
