bfe55a8d207194b5e202cd981a57537bcbb8b6b4
raffy-bekhit <raffybekhit@gmail.com>
2017-11-22 17:03:49 +0200

    fix segmentation fault error

--- row_threaded_matrix.c
+++ row_threaded_matrix.c
@@ -1,60 +1,144 @@
-
-#include <stdio.h>
+	#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define M 2
-#define K 2
-#define N 1
+#define M 2  //rows1
+#define K 2  //row2 col1
+#define N 1  //col2
 
 #define TYPE int
 
+typedef struct Parameters  //paramters passed to the function
+{
 
-int matrixA[M][K]= {{1,4}, {1,1}};  //hardcoded matrices for trail.
-int matrixB[K][N]= {{3},{4}};
-int matrixC[M][N];
-int matrixB_rows=K;
-int matrixB_columns = N;
 
-void* calculate_row(int* );                        //prototypes for the used functions.
+    int rowsA,rowsB,colsA,colsB;
+
+    TYPE ** matrixA ;
+    TYPE ** matrixB;
+    TYPE ** matrixC ;
+
+
+} Parameters;
+
+
+typedef struct Argument  //Argument passed to thread function
+{
+
+    Parameters * parameters;
+    int row; //thread row
+
+} Argument;
+
+
+void create_Parameters(Parameters*p , TYPE** matrix1,TYPE** matrix2,TYPE**matrix3,int rows1,int cols1,int rows2, int cols2)
+{
+    p->matrixA=matrix1;
+    p->matrixB=matrix2;
+    p->matrixC=matrix3;
+    p->rowsA=rows1;
+    p->colsA=cols1;
+    p->rowsB=rows2;
+    p->colsB=cols2;
+
+    return;
+}
+void create_Argument ( Argument * a , Parameters*p ,int row)
+{
+
+    a->parameters=p;
+    a->row=row;
+    return;
+
+}
+
+
+
+
+
+void* calculate_row(Argument* );
+void row_threaded_marix_mult(TYPE** ,TYPE** ,TYPE**,int ,int ,int , int ) ;  //prototypes for the used functions.
+
+
+void row_threaded_marix_mult(TYPE** matrixA,TYPE** matrixB,TYPE **matrixC,int rowsA,int colsA,int rowsB, int colsB)
+{
+    if(colsA!=rowsB) //checks matrices multiplication condition
+
+    {
+        printf("Matrices aren't of the same lengths"); //error to be displyed
+        return ; //return NULL pointer in case of error
+    }
+    Parameters * p = (Parameters*)malloc(sizeof(Parameters)); //allocates memory for Parameters
+
+
+
+    create_Parameters(p,matrixA,matrixB,matrixC,rowsA,colsA,rowsB,colsB); //initializes p with parameters given to function
+
+    Argument a[rowsA] ; //Arguments of thread function
+
+
+
+
+
+    int i,j; //loop for assigning a thread to each and every element that will be calculated in the new matrix
+    int error_flag;
+    pthread_t threads_array[rowsA]; //array of threads
+    pthread_attr_t attr_array[rowsA]; //array of thread attributes
+    int thread_row[rowsA];  //array of number of row of matrix A to be given as args to the thread function
+
+    
+
+
+    for(i = 0; i < rowsA; i++)
+    {
+
+        create_Argument(&a[i],p,i); //thread_args are the number of row of matrix A
+
+
+
+        pthread_attr_init(&attr_array[i]);    //get default attributes
+        error_flag = pthread_create(&threads_array[i],&attr_array[i],calculate_row,&a[i]); //create the
+
+        if(error_flag!=0)  //checks if thread is created .. if not returns NULL pointer
+        {
+            printf("error in thread creation: %d\n",error_flag);
+            return ;
+        }
+    }
+
+    for(i = 0; i < rowsA; i++)
+    {
+        pthread_join(threads_array[i],NULL);
+    }
+
+
+
+    free(p);
+    return;
+
+
+
+
+
+}
 
 
 int main()
 {
 
-    int i,j;                                               //loop for assigning a thread to each and every element that will be calculated in the new matrix
-    int error_flag;
-    pthread_t threads_array[M];
-    pthread_attr_t attr_array[M];
-    int rows[M];
+
+    int i,j;
+    TYPE matrixA[2][2] = {{1,2} , {3,4}} ;
+
+    TYPE matrixB[2][2] = {{3,4},{1,2}};
+    TYPE matrixC[2][2] ;
 
 
-    for(i = 0; i < M; i++)
-    {
-        rows[i]=i;
-
-        pthread_attr_init(&attr_array[i]);    //get default attributes
-        error_flag = pthread_create(&threads_array[i],&attr_array[i],calculate_row,&rows[i]); //create the
-
-        if(error_flag!=0)
-        {
-            printf("error in thread: %d\n",error_flag);
-            return -1;
-        }
-    }
-
-    for(i = 0; i < M; i++)
-    {
-      int temp =   pthread_join(threads_array[i],NULL);
-    }
-
-
-
-
+    row_threaded_marix_mult(matrixA,matrixB,matrixC,2,2,2,2);
 
 //Print out the resulting matrix
-    for(i = 0; i < M; i++)
+    for(i = 0; i < 2; i++)
     {
-        for(j = 0; j < N; j++)
+        for(j = 0; j< 2; j++)
         {
             printf("%d ", matrixC[i][j]);
         }
@@ -67,22 +151,38 @@ int main()
 }
 
 //function for calculating the value of element.
-void* calculate_row(int* row)
+void* calculate_row(Argument * a)
 {
 
     int i,j; //variables used in loops
 
-    int sum=0; //temporary variable to hold the value of the calculated element
+    TYPE sum=0; //temporary variable to hold the value of the calculated element
 
+    Parameters * p = a->parameters;
 
-    for(i=0; i< matrixB_columns; i++) //loops the columns of matrixB
+    TYPE** matrixA = p->matrixA ;
+    TYPE** matrixB = p->matrixB ;
+    TYPE** matrixC = p->matrixC ;
+
+    int colsA = p->colsA ;
+    int colsB = p->colsB ;
+    int rowsB = p->rowsB;
+
+    int row = a->row;
+
+    int x , y ;
+
+    for(i=0; i< colsB; i++) //loops the columns of matrixB
     {
 
-        for(j=0; j< matrixB_rows; j++) //loops rows of matrixB //loops columns of matrixA for a specific row
+        for(j=0; j< rowsB; j++) //loops rows of matrixB //loops columns of matrixA for a specific row
         {
-            sum = sum + (matrixA[*row][j] * matrixB[j][i]);
+          x=*((TYPE*)matrixA+(row*colsA)+j);
+          y = *((TYPE*)matrixB+(j*colsB)+i);
+           sum = sum +(x*y);
         }
-        matrixC[*row][i]=sum;
+        *((TYPE*)matrixC+(row*colsB)+i) = sum;
+
         sum=0;
     }
 
