9072494047dc7628c6020db63cf525edf4e56ccc
yomnabarakate21 <yomnabarakate21@gmail.com>
2017-11-23 20:45:44 +0200

    readMatrix fixed

--- readMatrix.c
+++ readMatrix.c
@@ -18,25 +18,33 @@ file=fopen(filename, "r");
 if (file == NULL)
     return 1;
 
-  
+   
     
 while ((read = getline(&line, &len, file)) != -1) {
-   int j=0;
+   
+   
+    int j=0;
     no_of_rows ++;
+   
+
+
+
     TYPE*t=split_elements(line);
-     
+    
    for(j=0;j<4;j++)
     {
         printf("%f  ",t[j]);
     }
     printf("\n");
-    free(t);
+   // free(t);
+ 
 }
 
 
 printf("this is the number of rows  %d",no_of_rows);
 if (line)
     free(line);
+
 
 
 }
@@ -59,36 +67,32 @@ int is_number(char* string) //check if string contains an alphabet
 }
 
 TYPE* split_elements(char* buffer) //returns number of columns or error number // column_num is number of columns already counted from previous turns
-{
+{      
     char splitter[]={'\t','\n',' '}; //used in strtok_r function
-    char * token ;
-         char * token2 ;     
-    char * position = buffer;
-    char* position2=buffer;
+    char * token ;    
+    char * position ;
+    position=buffer;
+  
+    
+   
 
     int column_c=0; //number of columns present in this turn
     //if(row_c>1) column_c=column_num;
 
-    TYPE * temp_row;
-     TYPE * row = malloc(100*sizeof(TYPE)); // allocates memory for the row
-int i=0;
- while( token = (char *)strtok_r(position,splitter,&position) )
-    {
-       // if(!isnumber(token))
-        //error  =-1;  //-1 indicates that non-digit character is present in matrix
-      //  puts(token);
-       // printf("%f\n",*(temp_row+column_c));
-       // *(temp_row+column_c)=atof(token);x
-        //temp_row++;
-        // printf("this is the number of columns %d\n",column_c);
+    int size = sizeof(TYPE);
+   TYPE* row = (TYPE*) malloc(size);
+     // allocates memory for the row
 
-        row[i]=atof(token);
-        printf("this is the token %f\n",row[i]);
-        i++;
+while( token = (char *)strtok_r(position,splitter,&position) )
+    {
+        row = (TYPE*) realloc(row,size);
+    
+       row[column_c] = atof(token);
+       size+=sizeof(TYPE);
+       
         column_c++;
     }
-    column_c ++;
-
+   
 
    // if(row_c>1&&(column_c!=column_num))
     //error = -2; //indicates that colums aren't equal in the same matrix
